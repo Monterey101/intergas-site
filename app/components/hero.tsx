6d8677eb99f4d88688c6d8e7f9549525cb0443a2
Monterey101 <kkoc0015@uni.sydney.edu.au>
2026-07-17 20:37:05 +1000

    fix: upstream dependency conflict

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
             <div className="absolute inset-0 flex flex-col items:center lg:items-start justify-center text-white font-bold">
                 <div className="flex flex-col gap-5 lg:gap-10 bg-black p-10 m-5">
                     <div className="flex justify-center items-center gap-0 sm:gap-6 text-6xl lg:text-9xl">
-                        <HeroLogo className="w-[0px] h-[0px] sm:w-[60px] sm:h-[60px] lg:w-[120px] lg:h-[120px] bg-blue-500" />
+                        <HeroLogo className="w-[0px] h-[0px] sm:w-[60px] sm:h-[60px] lg:w-[120px] lg:h-[120px]" />
                         INTERGAS
                     </div>
                     <div className="flex flex-col gap-10 lg:flex-row items-center justify-between text-center">
